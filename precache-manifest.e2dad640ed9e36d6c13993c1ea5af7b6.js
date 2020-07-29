e0a4de420e8890d5da878a174f3ff73796ca7de3
annavosbigian <annavosbigian2017@u.northwestern.edu>
2020-07-29 14:15:18 -0800

    Updates

---
+++ precache-manifest.e2dad640ed9e36d6c13993c1ea5af7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cbda2685d4b18c22734d6776322802",
+    "url": "/playlist-improv/index.html"
+  },
+  {
+    "revision": "30772cf15ed515d783f8",
+    "url": "/playlist-improv/static/css/main.8f3ed25b.chunk.css"
+  },
+  {
+    "revision": "f6aea061ca50c29b51a8",
+    "url": "/playlist-improv/static/js/2.71270da9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playlist-improv/static/js/2.71270da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30772cf15ed515d783f8",
+    "url": "/playlist-improv/static/js/main.bc97d557.chunk.js"
+  },
+  {
+    "revision": "7d2e265398cc524c1a2a",
+    "url": "/playlist-improv/static/js/runtime-main.61e00cac.js"
+  }
+]);
